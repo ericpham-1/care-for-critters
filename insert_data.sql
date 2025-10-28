25f57d30e57fd4c7ca302e2d2252c69e6c106ace
30205221 <erica.chow1@ucalgary.ca>
2025-10-28 11:14:47 -0600

    added dependencies for shelter and petid

--- insert_data.sql
+++ insert_data.sql
@@ -1,11 +1,16 @@
 USE 471_db;
 
-INSERT INTO Animal (Name, Description, Age, Diet, ShelterID)
+INSERT INTO Shelter (ShelterID, PhoneNumber, BuildingNo, Street, City, Province, PostalCode)
+VALUES
+    ('1', '1234567890', '143', 'Maple Street', 'Calgary', 'Alberta', 'A1B 3C4');
+
+
+INSERT INTO Animal (PetID, Name, Description, Age, Diet, ShelterID)
 VALUES 
-    ('Max', 'Friendly golden retriever', 4, 'Dog food', 1),
-    ('Whiskers', 'Playful tabby cat', 2, 'Cat food', 1),
-    ('Nemo', 'Clownfish', 1, 'Fish flakes', 1),
-    ('Spike', 'Bearded dragon', 3, 'Insects and veggies', 1);
+    (2, 'Max', 'Friendly golden retriever', 4, 'Dog food', 1),
+    (3,'Whiskers', 'Playful tabby cat', 2, 'Cat food', 1),
+    (4,'Nemo', 'Clownfish', 1, 'Fish flakes', 1),
+    (5,'Spike', 'Bearded dragon', 3, 'Insects and veggies', 1);
 
 INSERT INTO Mammal (PetID, Species, HealthRecords, Weight)
 VALUES 
